dd7f8f6edd56b1251f6793f676120d339f2ad45d
riskidwiputra <riskidwiputra1250@gmail.com>
2024-05-25 15:05:06 +0700

    perubahan

---
+++ resources/views/admin/layouts/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> {{ AmbilVersi() }}
+    </div>
+    <!-- <?= config_item('nama_aplikasi') ?> -->
+    <!-- <?= config_item('nama_aplikasi') ?> -->
+    <strong>Aplikasi <a href="https://github.com/OpenSID/OpenSID" target="_blank">Web Desa
+           </a>, dikembangkan oleh <a href="https://www.facebook.com/groups/OpenSID/"
+            target="_blank">Programmer Medan </a>.</strong>
+</footer>
